3ab42f53676c3d623d73995c4c6d8df97131e71f
Stephen Mhuli <stevemshible@gmail.com>
2022-04-04 20:50:30 +0300

    getting started

---
+++ app/components/ProductContainer.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+
+const ProductContainer=()=>{
+    return(
+        <View>
+            <Text>Product Container</Text>
+        </View>
+    )
+}
+
+export default ProductContainer;
