43990779447a2faa983ac4e26e08de75d18d92eb
Stephen Mhuli <stevemshible@gmail.com>
2022-04-08 23:55:33 +0300

    added auth

--- App.js
+++ App.js
@@ -1,30 +1,54 @@
 import 'react-native-gesture-handler';
 
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import ProductContainer from './app/components/ProductContainer';
 import Header from './app/shared/Header';
-import WelcomeSreen from './app/screens/WelcomeScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 import SignUpScreen from './app/screens/SignUpScreen';
 import SignInScreen from './app/screens/SignInScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState } from 'react';
 
+const Stack = createNativeStackNavigator();
+// style={styles.container}
 export default function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState(null);
+
+  if(!isLoading){
+    return(
+      <View style={styles.activity}>
+        <ActivityIndicator size="large"/>
+      </View>
+    );
+  }
   return (
       <NavigationContainer>
-        <View style={styles.container}>
+        {/* <Stack.Navigator screenOptions={{
+          // headerShown: false
+        }}>
+          <Stack.Screen options={{headerShown: false}} name="Home" component={WelcomeScreen} />
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          
+        </Stack.Navigator> */}
+
+        {/* <View style={styles.container}>
          {/* <WelcomeSreen /> */}
-         <SignInScreen />
+         {/* <SignInScreen /> */}
          {/* <SignUpScreen /> */}
-        </View>
+        {/* </View> */} 
       </NavigationContainer>  
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  activity: {
     flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
