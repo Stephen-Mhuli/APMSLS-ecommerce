f43f8a972879eaa95bb4f991c8136bb36502489f
Stephen Mhuli <stevemshible@gmail.com>
2022-04-08 22:06:51 +0300

    made changes

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
+import 'react-native-gesture-handler';
+
 import { StyleSheet, Text, View } from 'react-native';
 import ProductContainer from './app/components/ProductContainer';
 import Header from './app/shared/Header';
 import WelcomeSreen from './app/screens/WelcomeScreen';
 import SignUpScreen from './app/screens/SignUpScreen';
 import SignInScreen from './app/screens/SignInScreen';
-import { NativeBaseProvider } from 'native-base'
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+      <NavigationContainer>
         <View style={styles.container}>
          {/* <WelcomeSreen /> */}
-         {/* <SignInScreen /> */}
-         <SignUpScreen />
+         <SignInScreen />
+         {/* <SignUpScreen /> */}
         </View>
-    </NativeBaseProvider>
+      </NavigationContainer>  
   );
 }
 
