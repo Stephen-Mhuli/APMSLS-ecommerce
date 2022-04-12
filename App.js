be11d7b69e59ab05217bda74172e845bc4ab7a63
Stephen Mhuli <stevemshible@gmail.com>
2022-04-12 21:42:40 +0300

    added product screen

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
 import {  StyleSheet, Text, View } from 'react-native';
-import ProductContainer from './app/components/ProductContainer';
-import Header from './app/shared/Header';
+
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import SignUpScreen from './app/screens/SignUpScreen';
 import SignInScreen from './app/screens/SignInScreen';
+import ProductScreen from './app/screens/ProductScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useState, useEffect } from 'react';
 import { ActivityIndicator } from 'react-native-paper';
 
 
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [userToken, setUserToken] = useState(null);
 
-  // useEffect(()=>{
-  //   setTimeout(()=>{
-  //     setIsLoading(false);
-  //   },1000)
-  // },[]);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading(true);
+    },500)
+  },[]);
 
   if(!isLoading){
     return(
@@ -38,9 +39,11 @@ export default function App() {
           <Stack.Screen name="Welcome" component={WelcomeScreen} />
           <Stack.Screen name="SignUp" component={SignUpScreen} />
           <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="Productscreen" component={ProductScreen} />
           
         </Stack.Navigator>
       </NavigationContainer>  
+      
   );
 }
 
